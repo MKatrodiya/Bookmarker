e05e97932c94d028e4e5f21d9a49661c16cd3da2
Meet <katrodiya.m@northeastern.edu>
2025-07-19 14:59:45 -0400

    Added postgres for local dev

--- bookmarker-api/src/main/java/org/mk/bookmarker/BookmarkerApplication.java
+++ bookmarker-api/src/main/java/org/mk/bookmarker/BookmarkerApplication.java
@@ -1,7 +1,12 @@
 package org.mk.bookmarker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BookmarkerApplication {
